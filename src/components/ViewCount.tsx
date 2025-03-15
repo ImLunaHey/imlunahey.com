b8ad6aa4f00b3b24f1ee5688182f62ac46977563
luna <luna@wvvw.me>
2025-03-15 20:27:23 +0000

    fix: view count params

--- src/components/ViewCount.tsx
+++ src/components/ViewCount.tsx
@@ -2,7 +2,7 @@ import { useViewCount } from '../hooks/use-view-count';
 import { Loading } from './Loading';
 
 export const ViewCount = ({ rkey }: { rkey: string }) => {
-  const { data, isLoading } = useViewCount(rkey);
+  const { data, isLoading } = useViewCount({ rkey });
 
   if (isLoading) return <Loading />;
 
