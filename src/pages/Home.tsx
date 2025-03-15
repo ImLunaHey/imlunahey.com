110fc6b22200db225890fb19969a7921708d5bbb
luna <luna@wvvw.me>
2025-03-15 18:49:00 +0000

    fix: height on homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ export const HomePage = () => {
   return (
     <Page>
       <NavBar />
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center min-h-[calc(100vh-5rem)]">
         <h1 className="text-4xl font-bold">👋 hi, i'm luna</h1>
       </div>
     </Page>
