99ab0043b6228766b9efddf260fcec505bc09c21
luna <luna@wvvw.me>
2025-03-23 18:00:00 +0000

    fix: mobile size

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,3 +1,7 @@
 export const Page = ({ children }: { children: React.ReactNode }) => {
-  return <div className="max-w-screen-md mx-auto p-2">{children}</div>;
+  return (
+    <div className="w-full max-w-screen-md mx-auto p-2 min-h-[calc(100dvh-100px)]">
+      <div className="w-full">{children}</div>
+    </div>
+  );
 };
