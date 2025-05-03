94c1f06ee0ad87c84a8c9a29ba8033a8cc636533
luna <luna@wvvw.me>
2025-05-03 19:53:22 +0100

    fix: remove scrollbar from extra space

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router';
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100dvh-5rem)]">
+    <div className="flex min-h-[calc(100dvh-7rem)] flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">👋 hi, i'm luna</h1>
       <p className="text-lg">i'm a software engineer based in london.</p>
       <div className="flex flex-row gap-4">
