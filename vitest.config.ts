2dfc6752323e03d1a283d95fd0f828cec5ce27e3
luna <luna@wvvw.me>
2025-05-04 00:58:14 +0100

    fix: tailwind not loading in tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import { compareScreenshot } from './src/tests/commands/screenshot';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     setupFiles: ['./src/tests/setup/vitest.setup.ts', './src/tests/setup/matchers.ts'],
     browser: {
