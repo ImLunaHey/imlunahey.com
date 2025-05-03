91a66d0f14748b9af41a23652389d11c83ae4ae7
luna <luna@wvvw.me>
2025-05-03 19:21:09 +0100

    fix: debug button icon

--- src/components/DevTools.tsx
+++ src/components/DevTools.tsx
@@ -1,31 +1,64 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from 'react';
 
-const CustomDevTools = () => {
+const DebugModeToggle = () => {
+  const [enabled, setEnabled] = useState(false);
   return (
     <button
       onClick={() => {
         document.body.classList.toggle('debug');
+        setEnabled(!enabled);
       }}
       className="active:stroke-red/80 fixed bottom-4 left-4 z-50 rounded-full bg-black/50 p-2 text-white transition-all duration-150 hover:bg-black/70 active:scale-95"
+      title="Toggle Debug Mode"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        <path d="M12 2a10 10 0 1 0 10 10 4 4 0 0 1-5-5 4 4 0 0 1-5-5" />
-        <path d="M8.5 8.5v.01" />
-        <path d="M16 15.5v.01" />
-        <path d="M12 12v.01" />
-        <path d="M11 17v.01" />
-        <path d="M7 14v.01" />
-      </svg>
+      {enabled ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="m8 2 1.88 1.88" />
+          <path d="M14.12 3.88 16 2" />
+          <path d="M9 7.13v-1a3.003 3.003 0 1 1 6 0v1" />
+          <path d="M12 20c-3.3 0-6-2.7-6-6v-3a4 4 0 0 1 4-4h4a4 4 0 0 1 4 4v3c0 3.3-2.7 6-6 6" />
+          <path d="M12 20v-9" />
+          <path d="M6.53 9C4.6 8.8 3 7.1 3 5" />
+          <path d="M6 13H2" />
+          <path d="M3 21c0-2.1 1.7-3.9 3.8-4" />
+          <path d="M20.97 5c0 2.1-1.6 3.8-3.5 4" />
+          <path d="M22 13h-4" />
+          <path d="M17.2 17c2.1.1 3.8 1.9 3.8 4" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <path d="M15 7.13V6a3 3 0 0 0-5.14-2.1L8 2" />
+          <path d="M14.12 3.88 16 2" />
+          <path d="M22 13h-4v-2a4 4 0 0 0-4-4h-1.3" />
+          <path d="M20.97 5c0 2.1-1.6 3.8-3.5 4" />
+          <path d="m2 2 20 20" />
+          <path d="M7.7 7.7A4 4 0 0 0 6 11v3a6 6 0 0 0 11.13 3.13" />
+          <path d="M12 20v-8" />
+          <path d="M6 13H2" />
+          <path d="M3 21c0-2.1 1.7-3.9 3.8-4" />
+        </svg>
+      )}
     </button>
   );
 };
@@ -33,7 +66,7 @@ const CustomDevTools = () => {
 export const DevTools = () => {
   return (
     <>
-      <CustomDevTools />
+      <DebugModeToggle />
       <ReactQueryDevtools />
     </>
   );
